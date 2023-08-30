89d497cc7cf6220c61eff4c78485462690e2601d
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-30 12:07:07 +0700

    add search bar in main page, still need fix

--- src/App.js
+++ src/App.js
@@ -47,11 +47,15 @@ function App() {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-6 h-6 mr-2 text-white"
-                viewBox="0 0 50 50"
+                viewBox="0 0 50 50" 
               >
-                <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+                <path fill="#FFFFFF" d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
               </svg>
-              What do you want to see live?
+              <input
+                type="text"
+                placeholder="What do you want to see live?"
+                className="bg-white rounded-full px-4 py-2 w-full"
+              />
             </div>
           </div>
         </section>
@@ -73,9 +77,9 @@ function App() {
         <h2 className="text-xl font-bold">Categories</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           <div className="bg-black text-white rounded p-4">Concerts</div>
-          <div className="bg-black text-white rounded p-4">Concerts</div>
-          <div className="bg-black text-white rounded p-4">Concerts</div>
-          <div className="bg-black text-white rounded p-4">Concerts</div>
+          <div className="bg-black text-white rounded p-4">Sports</div>
+          <div className="bg-black text-white rounded p-4">Theatre</div>
+          <div className="bg-black text-white rounded p-4">Orchestra</div>
         </div>
       </section>
       <section className="p-4 md:p-20 gap-5">
@@ -168,7 +172,7 @@ function App() {
           </div>
           <div className="px-4 py-6 bg-gray-100 dark:bg-gray-900 md:flex md:items-center md:justify-between">
             <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-              © 2023 <a href="https://flowbite.com/">Purwadhika</a>. All Rights
+              © 2023 <a href="https://purwadhika.com/">Purwadhika</a>. All Rights
               Reserved.
             </span>
             <div className="flex mt-4 space-x-5 sm:justify-center md:mt-0">
