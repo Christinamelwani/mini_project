3686fad9cb3530d0638282458e2bb625d185181d
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-09-13 10:40:30 +0700

    add search by location and date features

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -10,8 +10,65 @@ import { useEffect } from 'react';
 const SearchPage = () => {
     const events = useSelector((state) => state.event.events);
     const [searchTerm, setSearchTerm] = useState('');
-    const filteredEvents = events?.filter(event => event.eventName.toLowerCase().includes(searchTerm));
-    const { search } = useLocation();
+    function parseDate(dateStr) {
+        const dateFormats = [
+            'yyyy-MM-dd',
+            'MM/dd/yyyy',
+            'dd-MM-yyyy'
+        ];
+    
+        for (let format of dateFormats) {
+            const date = Date.parse(dateStr, format);
+            if (!isNaN(date)) {
+                return new Date(date);
+            }
+        }
+        return null;
+    }
+    
+    function parseSearchTerm(searchTerm) {
+        const locationMatch = searchTerm.match(/\bin:\s*(.+?)(?=(\bfrom:|\bto:|$))/i);
+        const fromMatch = searchTerm.match(/\bfrom:\s*(\S+)/i);
+        const toMatch = searchTerm.match(/\bto:\s*(\S+)/i);
+    
+        let location = locationMatch ? locationMatch[1].trim() : "";
+        let dateFrom = fromMatch ? parseDate(fromMatch[1]) : null;
+        let dateTo = toMatch ? parseDate(toMatch[1]) : null;
+    
+        let query = searchTerm;
+        if (locationMatch) {
+            query = query.replace(locationMatch[0], "");
+        }
+        if (fromMatch) {
+            query = query.replace(fromMatch[0], "");
+        }
+        if (toMatch) {
+            query = query.replace(toMatch[0], "");
+        }
+    
+        query = query.trim();
+    
+        return { query, location, dateFrom, dateTo };
+    }
+    const { query, location, dateFrom, dateTo } = parseSearchTerm(searchTerm);
+    const filteredEvents = events?.filter(event => {
+        let matchesQuery = query ? event.eventName.toLowerCase().includes(query.toLowerCase()) : true;
+        let matchesLocation = location ? event.location.toLowerCase().includes(location.toLowerCase()) : true;
+        
+        let matchesDate = true;
+        if(dateFrom || dateTo) {
+            let eventDate = new Date(event.date);
+            if(dateFrom) {
+                matchesDate = matchesDate && eventDate >= dateFrom;
+            }
+            if(dateTo) {
+                matchesDate = matchesDate && eventDate <= dateTo;
+            }
+        }
+        
+        return matchesQuery && matchesLocation && matchesDate;
+    });
+        const { search } = useLocation();
     
     useEffect(() => {
         const params = new URLSearchParams(search);
